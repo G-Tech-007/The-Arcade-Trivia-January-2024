57f82df959ceb8ee4011ba0f1366f93699c351ed
G-Tech-007 <156540256+G-Tech-007@users.noreply.github.com>
2024-01-24 16:25:50 +0530

    GSP773_Eventarc_for_Cloud_Run1.sh

--- GSP773_Eventarc_for_Cloud_Run1.sh
+++ GSP773_Eventarc_for_Cloud_Run1.sh
@@ -47,18 +47,3 @@ gsutil mb -p $(gcloud config get-value project) \
   gs://${BUCKET_NAME}/
 
 
-echo "Hello World" > random.txt
-
-gsutil cp random.txt gs://${BUCKET_NAME}/random.txt
-
-gcloud beta eventarc attributes types describe google.cloud.audit.log.v1.written
-
-gcloud beta eventarc triggers create trigger-auditlog \
---destination-run-service=${SERVICE_NAME} \
---matching-criteria="type=google.cloud.audit.log.v1.written" \
---matching-criteria="serviceName=storage.googleapis.com" \
---matching-criteria="methodName=storage.objects.create" \
---service-account=${PROJECT_NUMBER}-compute@developer.gserviceaccount.com
-
-
-gsutil cp random.txt gs://${BUCKET_NAME}/random.txt
